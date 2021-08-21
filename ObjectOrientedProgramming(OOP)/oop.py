1080cf7248b4f8b3bb47d22890cca932146dac58
Bilal Karakan <85422865+BilalKarakan@users.noreply.github.com>
2021-08-21 12:26:38 +0300

    Add files via upload

---
+++ ObjectOrientedProgramming(OOP)/oop.py
@@ -0,0 +1,17 @@
+# Object Oriented Programming(OOP)
+
+# class
+# instance (object)
+
+lst1 = [1,2,3]
+lst2 = (1,2,3,4)
+list3 = {1:2, 3:4, 5:6}
+
+print(type(lst1))
+print(type(lst2))
+print(type(list3))
+
+# Class(Person) ==>  name, yearOfBirth, job, calculateAge()
+# Instances ==> sena, 1977, ,lawyer sena.calculateAge()
+# Instances ==> yigit, 2010, student, yigit.calculateAge()
+# Instances ==> emel, 1989, teacher, emel.calculateAge()
