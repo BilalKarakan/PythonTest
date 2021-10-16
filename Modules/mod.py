d286878f899ac830e838cc86d85182d7dbb1ebb9
Bilal Karakan <85422865+BilalKarakan@users.noreply.github.com>
2021-10-16 14:14:59 +0300

    Add files via upload

---
+++ Modules/mod.py
@@ -0,0 +1,24 @@
+"""
+Modül hakkında bilgilendirme
+"""
+
+print("Modül eklendi.")
+
+number = 10
+numbers = [1,2,3]
+
+person = {
+    "name" : "Bilal",
+    "age" : "22",
+    "city" : "İzmir" 
+}
+
+def func(x):
+    """
+    Fonksiyon hakkında bilgilenmdirme
+    """
+    print(f"x : {x}")
+
+class Person:
+    def speak(self):
+        print("I am speaking...")
