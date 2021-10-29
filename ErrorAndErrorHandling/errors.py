ea52df2610bac55da435ed5acfe3b8e798ca0440
Bilal Karakan <85422865+BilalKarakan@users.noreply.github.com>
2021-10-30 00:00:39 +0300

    Add files via upload

---
+++ ErrorAndErrorHandling/errors.py
@@ -0,0 +1,10 @@
+# Error => Hata
+
+#Error => https://docs.python.org/3/library/exceptions.html
+# print(a) => NameError
+# int("1a2") => ValueError
+# print("denem"e) => SyntaxError
+# print(10/0) => ZeroDivisionError
+
+# Error Handling => Hata Yönetimi
+
